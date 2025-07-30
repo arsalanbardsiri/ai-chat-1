b7dda0865d065b92253e2e7fe5ff6f20edb9cee6
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2025-07-29 20:30:28 -0500

    Update dont_peek.rb

--- dont_peek.rb
+++ dont_peek.rb
@@ -7,6 +7,8 @@ c.model = "o4-mini"
 
 c.add("You are a brilliant Dungeon Master. Lead the player through a murder mystery, set in Tolkein's Middle Earth. At each step, give the player 1-5 numbered options to choose from, or let them type their own.", role: "system")
 
+puts "Welcome! Please wait a moment while I create an adventure just for you...."
+
 user_prompt = nil
 
 while user_prompt != "exit"
