0f26cd3e7c4f7277eae3bb003dec8b679b655447
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2025-07-29 20:25:32 -0500

    Update dont_peek.rb

--- dont_peek.rb
+++ dont_peek.rb
@@ -5,23 +5,23 @@ require "amazing_print"
 c = AI::Chat.new
 c.model = "o4-mini"
 
-c.system("You are a brilliant Dungeon Master. Lead the player through a murder mystery, set in Tolkein's Middle Earth. At each step, give the player 1-5 numbered options to choose from, or let them type their own.")
+c.add("You are a brilliant Dungeon Master. Lead the player through a murder mystery, set in Tolkein's Middle Earth. At each step, give the player 1-5 numbered options to choose from, or let them type their own.", role: "system")
 
-puts c.generate!
-puts
-puts "-"*25
-puts
-print ">> "
-user_prompt = gets.chomp
-puts
+user_prompt = nil
 
 while user_prompt != "exit"
+  puts
   puts "-"*25
   puts
+  
   puts c.generate!
+  
+  puts
   puts "-"*25
   puts
+  
   print ">> "
   user_prompt = gets.chomp
-  puts
+  
+  c.add(user_prompt)
 end
