7cb5dd8387807666ac97ad10b4bef131eb764707
Arsalan Bardsiri <35473718+arsalanbardsiri@users.noreply.github.com>
2025-08-31 05:52:54 +0000

    AI chat library

--- app.rb
+++ app.rb
@@ -1,3 +1,22 @@
+require "bundler/setup"
 require "dotenv/load"
+require "ai-chat"
+require "amazing_print"
 
-open_ai = ENV['OPEN_AI']
+open_ai = ENV['OPENAI_API_KEY']
+
+c = AI::Chat.new
+
+c.system("You are a helpful assistant who talks like Shakespeare.")
+
+c.user("Hello! What are the best spots for pizza in Chicago?")
+
+ai_reply = c.generate!
+
+ap ai_reply
+
+ap c
+
+ap c.messages
+
+
